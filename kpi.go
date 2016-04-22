dfd367b4194df0724d1d80fd5b09da8389d3c6cf
Ollivier Robert <roberto@keltia.net>
2016-04-22 20:54:08 +0200

    Tentative computation of missing scores.

---
+++ kpi.go
@@ -0,0 +1,50 @@
+// kpi.go
+
+/*
+This file have func for computing the TLS scores
+ */
+package main
+
+const (
+	ProtocolTable = map[string]float32{
+		"SSL2.0": 0.0,
+		"SSL3.0": 80.0,
+		"TLS1.0": 90.0,
+		"TLS1.1": 95.0,
+		"TLS1.2": 100.0,
+	}
+)
+
+// getProtoScore merge proto + version and retrieve the score
+func getProtoScore(p *LabsProtocol) (score float32) {
+	proto := p.Name + p.Version
+	score = ProtocolTable[proto]
+	return
+}
+
+// protocolScore compute the relative ProcotolScore KPI
+func protocolScore(r *LabsReport) (score int) {
+	var (
+		lowest, highest float32
+	)
+
+	// check for SSL2.0
+	protoList := r.Endpoints[0].Details.Protocols
+
+	if len(protoList) == 1 {
+		score = getProtoScore(protoList[0])
+		return
+	}
+
+	for _, p := range protoList {
+		score = getProtoScore(p)
+		if score < lowest {
+			lowest = score
+		}
+		if score > highest {
+			highest = score
+		}
+	}
+	score = (lowest + highest) / 2
+	return
+}
