8211e1fa055a4dc6a1faeabc0445e332f86d37d0
Ollivier Robert <roberto@keltia.net>
2016-04-22 14:00:34 +0200

    Add two new options: more verbose and restrict to 1 site.

--- cli.go
+++ cli.go
@@ -5,9 +5,13 @@ package main
 import "flag"
 
 var (
+	fSiteName string
 	fVerbose bool
+	fReallyVerbose bool
 )
 
 func init() {
+	flag.StringVar(&fSiteName, "S", "", "Display that site")
 	flag.BoolVar(&fVerbose, "v", false, "Verbose mode")
+	flag.BoolVar(&fReallyVerbose, "V", false, "More verbose mode")
 }
