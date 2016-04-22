7c054c817aa9d34a9cf405cff78adc51e76145f0
Ollivier Robert <roberto@keltia.net>
2016-04-22 15:50:50 +0200

    Add -t parameter for labs/imirhil reports.

--- cli.go
+++ cli.go
@@ -5,12 +5,14 @@ package main
 import "flag"
 
 var (
+	fType string
 	fSiteName string
 	fVerbose bool
 	fReallyVerbose bool
 )
 
 func init() {
+	flag.StringVar(&fType, "t", "labs", "Type of report")
 	flag.StringVar(&fSiteName, "S", "", "Display that site")
 	flag.BoolVar(&fVerbose, "v", false, "Verbose mode")
 	flag.BoolVar(&fReallyVerbose, "V", false, "More verbose mode")
