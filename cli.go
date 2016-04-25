cf5e9cfa981c71d6fdc8200117a1efb7a500120b
Ollivier Robert <roberto@keltia.net>
2016-04-25 15:22:02 +0200

    Add -I option to avoid fetching tls.imirhil.fr status.

--- cli.go
+++ cli.go
@@ -7,6 +7,7 @@ import "flag"
 var (
 	fType string
 	fSiteName string
+	fIgnoreImirhil bool
 	fVerbose bool
 	fReallyVerbose bool
 )
@@ -14,6 +15,7 @@ var (
 func init() {
 	flag.StringVar(&fType, "t", "labs", "Type of report")
 	flag.StringVar(&fSiteName, "S", "", "Display that site")
+	flag.BoolVar(&fIgnoreImirhil, "I", false, "Do not fetch tls.imirhil.fr grade")
 	flag.BoolVar(&fVerbose, "v", false, "Verbose mode")
 	flag.BoolVar(&fReallyVerbose, "V", false, "More verbose mode")
 }
