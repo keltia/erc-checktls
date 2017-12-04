7b4018db861a1d81cd3ff6e99ec24c46776917b4
Ollivier Robert <roberto@keltia.net>
2017-12-04 13:23:20 +0100

    Distinguish between flag & func.

--- imirhil/utils.go
+++ imirhil/utils.go
@@ -4,7 +4,7 @@ import "log"
 
 // debug displays only if fVerbose is set
 func verbose(str string, a ...interface{}) {
-    if ctx.verbose {
+    if ctx.fVerbose {
         log.Printf(str, a...)
     }
 }
