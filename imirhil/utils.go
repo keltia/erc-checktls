c9d59ad4a0552d1f23257e9ac88c4dab1e100c14
Ollivier Robert <roberto@keltia.net>
2017-12-18 14:33:19 +0100

    Add debug().

--- imirhil/utils.go
+++ imirhil/utils.go
@@ -2,9 +2,16 @@ package imirhil
 
 import "log"
 
+// debug displays only if fDebug is set
+func debug(str string, a ...interface{}) {
+	if ctx.debug {
+		log.Printf(str, a...)
+	}
+}
+
 // debug displays only if fVerbose is set
 func verbose(str string, a ...interface{}) {
-    if ctx.fVerbose {
-        log.Printf(str, a...)
-    }
+	if ctx.verbose {
+		log.Printf(str, a...)
+	}
 }
--- utils.go
+++ utils.go
@@ -2,9 +2,16 @@ package main
 
 import "log"
 
+// debug displays only if fDebug is set
+func debug(str string, a ...interface{}) {
+	if fDebug {
+		log.Printf(str, a...)
+	}
+}
+
 // debug displays only if fVerbose is set
 func verbose(str string, a ...interface{}) {
-    if fVerbose {
-        log.Printf(str, a...)
-    }
+	if fVerbose {
+		log.Printf(str, a...)
+	}
 }
