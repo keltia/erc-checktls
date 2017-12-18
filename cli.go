352d8cf58c30053d8323f3d7f0183dcb139e42c7
Ollivier Robert <roberto@keltia.net>
2017-12-18 14:38:14 +0100

    Add -D debug flag.

--- cli.go
+++ cli.go
@@ -9,6 +9,7 @@ import (
 )
 
 var (
+	fDebug         bool
 	fType          string
 	fOutput        string
 	fSiteName      string
@@ -35,6 +36,7 @@ func init() {
 	flag.StringVar(&fType, "t", "text", "Type of report")
 	flag.StringVar(&fSiteName, "S", "", "Display that site")
 	flag.BoolVar(&fIgnoreImirhil, "I", false, "Do not fetch tls.imirhil.fr grade")
+	flag.BoolVar(&fDebug, "D", false, "Debug mode")
 	flag.BoolVar(&fVerbose, "v", false, "Verbose mode")
 	flag.BoolVar(&fReallyVerbose, "V", false, "More verbose mode")
 }
