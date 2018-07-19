27c2fd4a71c85cf01ab47706561ec0ffd46823f7
Ollivier Robert <roberto@keltia.net>
2018-07-19 16:38:10 +0200

    New types for the TLS report.

--- types.go
+++ types.go
@@ -2,12 +2,39 @@
 
 package main
 
-import "time"
+import (
+	"time"
+)
 
 // TLSReport is one single run for all sites
 type TLSReport struct {
-	EngineVersion   string
-	CriteriaVersion string
-	Date            time.Time
-	Sites           [][]string
+	SSLLabs string
+	Date    time.Time
+	Sites   []TLSSite
+}
+
+// TLSSite is a summary for each site
+type TLSSite struct {
+	Name     string
+	Contract string
+
+	Grade      string
+	CryptCheck string
+	Mozilla    string
+
+	DefKey bool
+	DefSig bool
+	DefCA  bool
+
+	IsExpired bool
+
+	Protocols string
+
+	RC4     bool
+	PFS     bool
+	OCSP    bool
+	HSTS    bool
+	ALPN    bool
+	Drown   bool
+	Sweet32 bool
 }
