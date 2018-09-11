848bb24ccfbd0d8927079125845d426970a52fad
Ollivier Robert <roberto@keltia.net>
2018-09-11 23:21:15 +0200

    One more tests.

--- report_test.go
+++ report_test.go
@@ -16,6 +16,22 @@ func TestNewTLSReport(t *testing.T) {
 	require.Nil(t, rep)
 }
 
+func TestNewTLSReport2(t *testing.T) {
+	ji, err := ioutil.ReadFile("testdata/site.json")
+	require.NoError(t, err)
+
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = true
+
+	all, err := ssllabs.ParseResults(ji)
+	require.NoError(t, err)
+
+	sites, err := NewTLSReport(all)
+	require.NoError(t, err)
+	require.NotEmpty(t, sites)
+}
+
 func TestTLSReport_ToCSV(t *testing.T) {
 	var buf strings.Builder
 
