e6bdfda70519b833587dc606bac2a5eca8c50154
Ollivier Robert <roberto@keltia.net>
2018-09-11 23:43:40 +0200

    More tests, now for site.go.

--- site_test.go
+++ site_test.go
@@ -1,10 +1,62 @@
 package main
 
 import (
+	"io/ioutil"
 	"testing"
 
+	"github.com/keltia/ssllabs"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestNewTLSSite(t *testing.T) {
+	ji, err := ioutil.ReadFile("testdata/site.json")
+	require.NoError(t, err)
+
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = true
+
+	all, err := ssllabs.ParseResults(ji)
+	require.NoError(t, err)
+
+	tls := NewTLSSite(all[0])
+	require.NotEmpty(t, tls)
+}
+
+func TestNewTLSSite1(t *testing.T) {
+	tls := NewTLSSite(ssllabs.Host{})
+	require.Empty(t, tls)
+}
+
+func TestInitAPI(t *testing.T) {
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = true
+	initAPIs()
+
+}
+
+func TestInitAPI1(t *testing.T) {
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = false
+	initAPIs()
+}
+
+func TestInitAPI2(t *testing.T) {
+	// Simulate
+	fIgnoreMozilla = false
+	fIgnoreImirhil = true
+	initAPIs()
+}
+
+func TestInitAPI3(t *testing.T) {
+	// Simulate
+	fIgnoreMozilla = false
+	fIgnoreImirhil = false
+	initAPIs()
+}
 
 func TestTLSSite_HasExpiredTrue(t *testing.T) {
 	tm := int64(1536423013000)
@@ -14,4 +66,33 @@ func TestTLSSite_HasExpiredTrue(t *testing.T) {
 func TestTLSSite_HasExpiredFalse(t *testing.T) {
 	tm := int64(1855828800000)
 	assert.False(t, hasExpired(tm))
+}
+
+func TestDisplayWildcards(t *testing.T) {
+	ji, err := ioutil.ReadFile("testdata/site.json")
+	require.NoError(t, err)
+
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = true
+
+	all, err := ssllabs.ParseResults(ji)
+	require.NoError(t, err)
+
+	str := displayWildcards(all)
+	assert.NotEmpty(t, str)
+}
+
+func TestSweet32(t *testing.T) {
+	ji, err := ioutil.ReadFile("testdata/reallybad.json")
+	require.NoError(t, err)
+
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = true
+
+	bad, err := ssllabs.ParseResults(ji)
+	require.NoError(t, err)
+
+	assert.True(t, checkSweet32(bad[0].Endpoints[0].Details))
 }
