f2222cffab0f01be3deffe77960904f408848ae9
Ollivier Robert <roberto@keltia.net>
2018-09-11 16:43:51 +0200

    Move tests at the right place.

--- html_test.go
+++ html_test.go
@@ -15,3 +15,15 @@ func TestBooleanF(t *testing.T) {
 	assert.Equal(t, white("FALSE"), booleanF(false))
 	assert.Equal(t, red("TRUE"), booleanF(true))
 }
+
+func TestRed(t *testing.T) {
+	str := ""
+	out := red(str)
+	assert.Equal(t, `<td class=xl64 align=center></td>`, out)
+}
+
+func TestRed1(t *testing.T) {
+	str := "foobar"
+	out := red(str)
+	assert.Equal(t, `<td class=xl64 align=center>foobar</td>`, out)
+}
