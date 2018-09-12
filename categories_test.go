ebb7853a2bb7899c0fe542bad884d146c226fbf9
Ollivier Robert <roberto@keltia.net>
2018-09-13 00:53:36 +0200

    One more test for categoryCounts().

--- categories_test.go
+++ categories_test.go
@@ -108,3 +108,24 @@ func TestCategoryCountsReport(t *testing.T) {
 	assert.NotEmpty(t, cntrs)
 	assert.EqualValues(t, good, cntrs)
 }
+
+func TestCategoryCountsReportDES(t *testing.T) {
+	ji, err := ioutil.ReadFile("testdata/reallybad.json")
+	require.NoError(t, err)
+
+	// Simulate
+	fIgnoreMozilla = true
+	fIgnoreImirhil = true
+
+	all, err := ssllabs.ParseResults(ji)
+	require.NoError(t, err)
+
+	good := map[string]int{
+		"OCSP": 1, "Total": 1, "X": 0, "Issues": 1,
+		"HSTS": 1, "Z": 0, "A+": 1, "PFS": 1, "Sweet32": 1,
+	}
+
+	cntrs := categoryCounts(all)
+	assert.NotEmpty(t, cntrs)
+	assert.EqualValues(t, good, cntrs)
+}
