c5f6624759302262fc83b91f69fe2e7f935574aa
Ollivier Robert <roberto@keltia.net>
2018-09-12 11:18:01 +0200

    Tests for readContractFile() and checkOutput().

--- main_test.go
+++ main_test.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
+	"path"
 	"testing"
+
+	"github.com/gobuffalo/packr"
 
 	"github.com/stretchr/testify/assert"
 
@@ -23,4 +27,35 @@ func TestGetResultsNothing(t *testing.T) {
 	buf, err := getResults("testdata/site.nowhere")
 	require.Error(t, err)
 	require.Empty(t, buf)
+}
+
+func TestReadContractFile(t *testing.T) {
+	// We embed the file now
+	box := packr.NewBox("./files")
+
+	cntrs, err := readContractFile(box)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, cntrs)
+}
+
+func TestCheckOutput(t *testing.T) {
+	fh := checkOutput("")
+	assert.NotEmpty(t, fh)
+	assert.EqualValues(t, os.Stdout, fh)
+}
+
+func TestCheckOutput_1(t *testing.T) {
+	temp, err := ioutil.TempDir("", "test")
+	require.NoError(t, err)
+
+	defer os.RemoveAll(temp)
+
+	fn := path.Join(temp, "foo.out")
+	fh := checkOutput(fn)
+	assert.NotEmpty(t, fh)
+
+	fi, err := os.Stat(fn)
+	assert.NoError(t, err)
+	assert.NotNil(t, fi)
+	assert.NotEmpty(t, fi)
 }
