7026cf8ea4f542a2e18f9c84f399ff7e01567326
Ollivier Robert <roberto@keltia.net>
2018-09-12 18:05:34 +0200

    Small scale tests for writeHTMLSummary().

--- summaries_test.go
+++ summaries_test.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -36,4 +38,43 @@ func TestWriteSummary2(t *testing.T) {
 	}
 	err := writeSummary(&buf, keys, cntrs)
 	require.NoError(t, err)
+}
+
+func TestWriteHTMLSummaryEmpty(t *testing.T) {
+	var buf strings.Builder
+
+	cntrs := map[string]int{}
+
+	err := writeHTMLSummary(&buf, ctlsmap, cntrs)
+	assert.NoError(t, err)
+	assert.Empty(t, buf)
+}
+
+func TestWriteHTMLSummary(t *testing.T) {
+	var buf strings.Builder
+
+	cntrs := map[string]int{
+		"A": 666,
+		"B": 42,
+	}
+
+	err := writeHTMLSummary(&buf, ctlsmap, cntrs)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, buf)
+	t.Logf("buf=%s", buf.String())
+}
+
+func TestWriteHTMLSummary_1(t *testing.T) {
+	var buf strings.Builder
+
+	cntrs := map[string]int{
+		"A": 666,
+		"B": 42,
+		"F": 1,
+	}
+
+	err := writeHTMLSummary(&buf, httpmap, cntrs)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, buf)
+	t.Logf("buf=%s", buf.String())
 }
