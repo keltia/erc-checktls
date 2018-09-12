3a24e69060c7d7aa2be38b47d62d385ac55d8618
Ollivier Robert <roberto@keltia.net>
2018-09-12 18:06:16 +0200

    WIP: core of writeHTMLSummary(), not complete.

--- summaries.go
+++ summaries.go
@@ -1,12 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 
 	tw "github.com/olekukonko/tablewriter"
 	"github.com/pkg/errors"
 )
+
+type cmap struct {
+	elem string
+	fn   func(string) string
+}
+
+var ctlsmap = []cmap{
+	{"A+", green},
+	{"A", green},
+	{"A-", yellow},
+	{"B", orange},
+	{"C", orange},
+	{"D", orange},
+	{"E", orange},
+	{"F", orange},
+	{"T", orange},
+	{"X", orange},
+	{"Z", orange},
+	{"Total", white},
+	{"Issues", white},
+	{"PFS", white},
+	{"OCSP", white},
+	{"HSTS", white},
+	{"Sweet32", white},
+}
+
+var httpmap = []cmap{
+	{"A+", green},
+	{"A", green},
+	{"A-", yellow},
+	{"B-", orange},
+	{"B", orange},
+	{"B-", orange},
+	{"C+", orange},
+	{"C", orange},
+	{"C-", orange},
+	{"D+", red},
+	{"D", red},
+	{"D-", red},
+	{"E+", red},
+	{"E", red},
+	{"E-", red},
+	{"F+", red},
+	{"F", red},
+	{"F-", red},
+	{"T", red},
+	{"X", red},
+	{"Z", red},
+	{"Total", white},
+	{"Broken", white},
+}
 
 func writeSummary(w io.Writer, keys []string, cntrs map[string]int) (err error) {
 	table := tw.NewWriter(w)
@@ -28,6 +80,16 @@ func writeSummary(w io.Writer, keys []string, cntrs map[string]int) (err error) 
 	return errors.Wrap(err, "table")
 }
 
-func writeHTMLSummary(w io.Writer, keys []string, cntrs map[string]int) (err error) {
+func writeHTMLSummary(w io.Writer, keys []cmap, cntrs map[string]int) (err error) {
+	var str bytes.Buffer
+
+	if len(keys) == 0 || len(cntrs) == 0 {
+		return nil
+	}
+	for _, cm := range keys {
+		str.WriteString(cm.fn(fmt.Sprintf("%d", cntrs[cm.elem])))
+		str.WriteString("\n")
+	}
+	w.Write(str.Bytes())
 	return nil
 }
